60324973e75fa7a1c77a9934fb5c42677fdf7a9d
SC-SOFT <windchild@vip.qq.com>
2016-12-26 11:29:34 +0800

    添加获取KEY方法

---
+++ RedisClient_BaiCh/RedisCmdReturnKeys.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+
+namespace RedisClient_BaiCh
+{
+    public class RedisCmdReturnKeys: RedisCmdReturn
+    {
+        public List<string> Keys { get; set; }
+
+        public RedisCmdReturnKeys(CommandMethodReturn commandMethodReturn) : base(commandMethodReturn)
+        {
+            Keys = commandMethodReturn.Arrays.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+    }
+}
